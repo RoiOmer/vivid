37b137ab49cb5a01426488f62a55d6ef3d5c56f2
RoiOmer <omerroye@gmail.com>
2025-05-12 18:30:23 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    document.getElementById('loading-screen').style.display = 'none';
+    const main = document.getElementById('main-content');
+    main.classList.remove('hidden');
+    main.classList.add('visible');
+  }, 3500); // 3.5 seconds coral animation
+});
